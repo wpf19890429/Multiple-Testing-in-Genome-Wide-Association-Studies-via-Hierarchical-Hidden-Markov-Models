525ee8b7b2591a7b476935a32fd0fab2ae28e790
wpf19890429 <42342718+wpf19890429@users.noreply.github.com>
2022-12-19 17:22:19 +0800

    Add files via upload

---
+++ rdata.HHMM.R
@@ -0,0 +1,75 @@
+rdata.HHMM<-function(NUM, S, pii_eta, pii_theta, A, B, f0, pc, f1)
+{
+
+#############################
+
+## Parameters
+ # NUM: the number of hypotheses
+ # S: the size of the block
+ # pii_eta: the initial probabilities of eta
+ # pii_theta: the initial probabilities of theta, K*1
+ # A: the transition matrix of theta
+ # B: the block-wide transition matrix of eta 
+ # f0: the pdf of z_i given theta_i=0
+ # pc: (pc[1], ..., pc[L]): proportion of mixture components
+ # f1: the pdf of z_i given theta_i=1
+
+
+## Values
+ # theta: the states of the null hypotheses
+ # eta: the class of region
+ # z: the observations
+
+###############################
+
+## NUM is a multiple of block_size
+	eta<-rep(0, NUM)
+	theta<-rep(0, NUM)
+	z<-rep(0, NUM)
+
+
+## generating the states
+
+ # initial state
+      K<-length(pii_eta)
+      eta[1:S]<-rep(sample(1:K, 1, replace=FALSE, prob=pii_eta), S)
+      theta[1]<-rbinom(1, 1, prob=pii_theta[eta[1]])
+
+ # other states
+
+      NUM_b<-NUM/S
+	for(i in 2:NUM_b)
+	{
+		block_start<-(i-1)*S+1
+            block_end<-i*S
+		eta[block_start:block_end]<-rep(sample(1:K, 1, replace=FALSE, prob=B[eta[block_start-1], ]), S)	
+	}
+	for(i in 2:NUM)
+	{
+		if(theta[i-1]==0)
+			theta[i]<-rbinom(1, 1, A[1, 2, eta[i]])
+		else
+			theta[i]<-rbinom(1, 1, A[2, 2, eta[i]])
+	}
+
+
+
+## generating the observations
+
+	L<-length(pc)
+	for (i in 1:NUM)
+	{
+		if (theta[i]==0)
+			z[i]<-rnorm(1, mean=f0[1], sd=f0[2])
+		else
+		{ 
+           		k<-sample(1:L, size=1, replace = FALSE, prob=pc)
+			z[i]<-rnorm(1, mean=f1[k, 1], sd=f1[k, 2])
+		}
+	}
+
+	data<-list(theta=theta, eta=eta, z=z)
+	return(data)
+
+}
+
